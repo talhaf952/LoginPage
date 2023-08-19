d3e178182773acc9fe86e9ad82449857014f1c45
Dell <talhaf952@gmail.com>
2023-08-19 09:05:44 -0800

    HeadingBorder

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -29,15 +29,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                    Intent intent=new Intent(MainActivity.this,secondactivity.class);
+                    Intent intent = new Intent(MainActivity.this, secondactivity.class); // Correct the class name
                     startActivity(intent);
-                }else{
+                } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
-
-
-
             }
         });
+
     }
 }
