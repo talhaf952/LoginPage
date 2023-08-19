ee09b3f892bdbcb1c4a0c0d303f1ca2b11343eb6
Dell <talhaf952@gmail.com>
2023-08-19 10:06:41 -0800

    Done

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MainActivity.this, secondactivity.class); // Correct the class name
-                    startActivity(intent);
+                    {Intent intent = new Intent(MainActivity.this, secondactivity.class); // Correct the class name
+                    startActivity(intent);}
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
