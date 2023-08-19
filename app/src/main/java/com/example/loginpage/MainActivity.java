ca0a7a3ea828542a26aac5fd1d8c92901f7958d0
Dell <talhaf952@gmail.com>
2023-08-18 18:29:45 -0800

    Shift_activity

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,9 +29,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
                     Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(MainActivity.this,secondactivity.class);
+                    startActivity(intent);
                 }else{
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
+
+
+
             }
         });
     }
